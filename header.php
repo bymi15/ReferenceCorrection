e6c29994666c55c12fbce911c0a915645ba3896a
Brian Min <bymi15@yahoo.com>
2016-04-02 20:31:39 +0100

    integrated new bootstrap theme and added most-viewed-posts section in index.php

--- header.php
+++ header.php
@@ -1,45 +1,65 @@
-<div class="header">
-    <h1 class="mainhead">Reference<span style="color:#0ca3d2">Checker</span></h1>
-    <?php
-    if (login_check($mysqli) == true) {
-        echo '<p>Welcome ' . htmlentities($_SESSION['username']) . '</p>';
-        echo '<a href="process_logout.php"><input type="button" value="Logout" class="submit_button"/></a>';
-    } else {
-        echo '<a href="login.php"><input type="button" value="Login" class="submit_button"/></a>';
-        echo '<a href="register.php"><input type="button" value="Register" class="submit_button"/></a>';
-    }
-    ?>
-</div>
-<div class="navigation">
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="about.php">About</a></li>
-        <li>
-            <a href="#">Categories</a>
-            <ul>
-                <li><a href="#">Cardiology &amp; Vascular Medicine</a></li>
-                <li><a href="#">Endocrinology</a></li>
-                <li><a href="#">Gastroenterology</a></li>
-                <li><a href="#">Genetics &amp; Genomics</a></li>
-                <li><a href="#">Haematology</a></li>
-                <li><a href="#">Infectious Diseases</a></li>
-                <li><a href="#">Neurology</a></li>
-                <li><a href="#">Obstetrics &amp; Gynaecology</a></li>
-                <li><a href="#">Oncology</a></li>
-                <li><a href="#">Paediatrics</a></li>
-                <li><a href="#">Psychiatry</a></li>
-                <li><a href="#">Public Health</a></li>
-                <li><a href="#">Respiratory Medicine</a></li>
-                <li><a href="#">Urology</a></li>
-                <li><a href="#">Other</a></li>
+<div class="first-nav-bar">
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-home" style="color:white"></span> <span style="color:white">Reference</span><span style="color:#0ca3d2">Checker</span></a>
+            <ul class="nav navbar-nav navbar-right">
+                <?php
+                if (login_check($mysqli) == true) {
+                    echo '<span class="navbar-brand">Welcome ' . htmlentities($_SESSION['username']) . "</span>";
+                    echo ' <a href="process_logout.php" class="btn btn-primary btn-sm">Logout</a>';
+                } else {
+                    echo '<a href="login.php" class="btn btn-primary btn-sm">Login</a>
+                        <a href="register.php" class="btn btn-primary btn-sm">Register</a>';
+                }
+                ?>
             </ul>
-        </li>
-        <li><a href="create_post.php">Post</a></li>
-    </ul>
-    <div class="search_bar">
-        <form method="get" action="search.php">
-            <input type="text" name="search" placeholder="Search...">
-            <button type="button" onclick="form.submit();">Search</button>
-        </form>
-    </div>
+        </div>
+    </nav>
 </div>
+
+<!-- Navigation bar and Website icon -->
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <!-- <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-home"></span> ReferenceChecker</a> -->
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-left">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Categories
+                        <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <?php
+                                include_once '/include/functions.php';
+                                $categories = getCategories();
+                                for($i = 0; $i < count($categories); $i++){
+                                    echo'
+                                        <li><a href="/category.php?id=' . $i . '">' . $categories[$i] . '</a>
+                                        </li>
+                                    ';
+                                }
+                            ?>
+                        </ul>
+                    </li>
+                    <li><a href="create_post.php">Post</a></li>
+                </ul>
+                <div class="col-sm-3 col-md-3 pull-right">
+                    <form class="navbar-form" role="search" method="get" action="search.php">
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Search" name="search" id="search">
+                            <div class="input-group-btn">
+                                <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </nav>
--- index.php
+++ index.php
@@ -7,91 +7,191 @@ my_session_start();
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
     <title>Reference Checker: Home</title>
 
-    <link rel="stylesheet" type="text/css" href="/css/index.css">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <!-- Main -->
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <!-- Hover.css -->
+    <link rel="stylesheet" type="text/css" href="css/hover.css">
+    <!-- Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Alegreya+Sans' rel='stylesheet' type='text/css'>
 </head>
 <body>
-    <div class="container">
+    <?php
+    include 'header.php';
+    include 'carousel.php';
+    ?>
+
+    <!-- Recent posts section -->
+     <div class="post_section container">
+        <a name="recent-posts"></a>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+              <div class="panel-title"><h4 style="font-weight:bold">Recent Posts</h4></div>
+            </div>
+
+        <!-- PHP CODE - RETRIEVES THE POSTS FROM THE DATABASE -->
         <?php
-        include 'header.php';
-        ?>
-        <div class="about_section">
-            <h3>Welcome to Reference Checker!</h3>
-            <p>Reference Checker description...</p>
-        </div>
-        <div class="post_section">
-            <h4>Recent posts</h4><hr>
+        include_once '/include/connection.php';
 
-                <!-- PHP CODE - RETRIEVES THE POSTS FROM THE DATABASE -->
-                <?php
-                include_once '/include/connection.php';
+        $sql = "SELECT posts.id, posts.article_title, posts.article_author, posts.date, posts.category, posts.author, posts.views, users.username FROM posts LEFT JOIN users ON posts.author = users.id ORDER BY date DESC LIMIT 250;";
 
-                $sql = "SELECT posts.id, posts.article_title, posts.article_author, posts.date, posts.category, posts.author, users.username FROM posts LEFT JOIN users ON posts.author = users.id ORDER BY date DESC LIMIT 10;";
+        $result = mysqli_query($mysqli, $sql);
 
-                $result = mysqli_query($mysqli, $sql);
-
-                if(!$result)
-                {
-                    echo '<h3>Could not display the recent posts. Please refresh the page and try again later.</h3>';
-                }
-                else
-                {
-                    if(mysqli_num_rows($result) == 0)
-                    {
-                        echo '<h3>No posts to be displayed.</h3>';
-                    }
-                    else
-                    {
-                        //for each post
-                        while($row = mysqli_fetch_assoc($result))
-                        {
-                            echo '<div class="post" onclick="location.href=\'post.php?id=' . $row['id'] . '\';">
-                                    <p class="post_title">"' . $row['article_title'] . '" <span style="font-weight: normal;">by ' . $row['article_author'] . '</span></p>
-                                    <p class="date">' . $row['date'] . '</p>
-                                    <p>category: <a href="#">' . $row['category'] . '</a> posted by: <a href="#">' . $row['username'] . '</a></p>
-                                </div>';
-                        }
+        if(!$result)
+        {
+            echo '<h3>Could not display the recent posts. Please refresh the page and try again later.</h3>';
+        }
+        else
+        {
+            if(mysqli_num_rows($result) == 0)
+            {
+                echo '<h3>No posts to be displayed.</h3>';
+            }
+            else
+            {
+                //pagination
+                $page = 1;
+                if(isset($_GET['recent_page'])) {
+                    $page = filter_input(INPUT_GET, 'recent_page', FILTER_VALIDATE_INT);
+                    if($page === false) {
+                        $page = 1;
                     }
                 }
-                ?>
+                $items_per_page = 5;
+                $offset = ($page - 1) * $items_per_page;
 
-                <!--
+                $row_count = mysqli_num_rows($result);
+                $page_count = 0;
+                if ($row_count !== 0){
+                    $page_count = (int)ceil($row_count / $items_per_page);
+                    if($page > $page_count) {
+                        $page = 1;
+                    }
+                }
+                echo '<ul class="pagination" style="margin-bottom: 1px; margin-top: 5px;">';
+                for ($i = 1; $i <= $page_count; $i++) {
+                    if ($i === $page) { // this is current page
+                        echo '<li class="active"><a href="#recent-posts">' . $i . '</a></li>';
+                    } else { // show link to other page
+                        echo '<li><a href="/index.php?recent_page=' . $i . '#recent-posts">' . $i . '</a></li>';
+                    }
+                }
+                echo '</ul>';
 
-                <div class="post" onclick="location.href='#';">
-                    <p class="post_title">Title of article 1</p>
-                    <p class="date">24/2/2015</p>
-                    <p>category: <a href="#">category</a> author: <a href="#">name</a></p>
-                </div>
-                <div class="post" onclick="location.href='#';">
-                    <p class="post_title">Title of article 2</p>
-                    <p class="date">24/2/2015</p>
-                    <p>category: <a href="#">category</a> author: <a href="#">name</a></p>
-                </div>
-                <div class="post" onclick="location.href='#';">
-                    <p class="post_title">Title of article 3</p>
-                    <p class="date">24/2/2015</p>
-                    <p>category: <a href="#">category</a> author: <a href="#">name</a></p>
-                </div>
-                <div class="post" onclick="location.href='#';">
-                    <p class="post_title">Title of article 4</p>
-                    <p class="date">24/2/2015</p>
-                    <p>category: <a href="#">category</a> author: <a href="#">name</a></p>
-                </div>
-                <div class="post" onclick="location.href='#';">
-                    <p class="post_title">Title of article 5</p>
-                    <p class="date">24/2/2015</p>
-                    <p>category: <a href="#">category</a> author: <a href="#">name</a></p>
-                </div>
-                <div class="post" onclick="location.href='#';">
-                    <p class="post_title">Title of article 6</p>
-                    <p class="date">24/2/2015</p>
-                    <p>category: <a href="#">category author: <a href="#">name</a></p>
-                </div>
-                -->
-        </div>
-    </div>
+                echo '<hr>';
+
+                $rows = array();
+                while($row = mysqli_fetch_assoc($result)){
+                    $rows[] = $row;
+                }
+
+                for($i = $offset; $i < $items_per_page + $offset; $i++){
+                    if(empty($rows[$i])) continue;
+
+                    echo '<div class="post hvr-overline-from-left" onclick="location.href=\'post.php?id=' . $rows[$i]['id'] . '\';">
+                    <p class="post_title"><span class="glyphicon glyphicon-stop" style="margin-right:5px"></span>"' . $rows[$i]['article_title'] . '" <span style="font-weight: normal;">by ' . $rows[$i]['article_author'] . '</span></p>
+                    <p class="date">' . $rows[$i]['date'] . '</p>
+                    <p><span class="glyphicon glyphicon-th-list"></span> category: <a href="#">' . $rows[$i]['category'] . '</a><span class="glyphicon glyphicon-user"></span> author: <a href="#">' . $rows[$i]['username'] . '</a></p>
+                    <p><span class="glyphicon glyphicon-eye-open"></span> views: ' . $rows[$i]['views'] . '</p>
+                        </div>';
+                }
+            }
+        }
+        echo'</div></div>';
+    ?>
+
+    <!-- Most viewed posts section -->
+     <div class="post_section container">
+        <a name="most-viewed-posts"></a>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+              <div class="panel-title"><h4 style="font-weight:bold">Most Viewed Posts</h4></div>
+            </div>
+
+        <?php
+        include_once '/include/connection.php';
+
+        $sql = "SELECT posts.id, posts.article_title, posts.article_author, posts.date, posts.category, posts.author, posts.views, users.username FROM posts LEFT JOIN users ON posts.author = users.id ORDER BY views DESC LIMIT 250;";
+
+        $result = mysqli_query($mysqli, $sql);
+
+        if(!$result)
+        {
+            echo '<h3>Could not display the recent posts. Please refresh the page and try again later.</h3>';
+        }
+        else
+        {
+            if(mysqli_num_rows($result) == 0)
+            {
+                echo '<h3>No posts to be displayed.</h3>';
+            }
+            else
+            {
+                //pagination
+                $page = 1;
+                if(isset($_GET['views_page'])) {
+                    $page = filter_input(INPUT_GET, 'views_page', FILTER_VALIDATE_INT);
+                    if($page === false) {
+                        $page = 1;
+                    }
+                }
+                $items_per_page = 5;
+                $offset = ($page - 1) * $items_per_page;
+
+                $row_count = mysqli_num_rows($result);
+                $page_count = 0;
+                if ($row_count !== 0){
+                    $page_count = (int)ceil($row_count / $items_per_page);
+                    if($page > $page_count) {
+                        $page = 1;
+                    }
+                }
+                echo '<ul class="pagination" style="margin-bottom: 1px; margin-top: 5px;">';
+                for ($i = 1; $i <= $page_count; $i++) {
+                    if ($i === $page) { // this is current page
+                        echo '<li class="active"><a href="#most-viewed-posts">' . $i . '</a></li>';
+                    } else { // show link to other page
+                        echo '<li><a href="/index.php?views_page=' . $i . '#most-viewed-posts">' . $i . '</a></li>';
+                    }
+                }
+                echo '</ul>';
+
+                echo '<hr>';
+
+                $rows = array();
+                while($row = mysqli_fetch_assoc($result)){
+                    $rows[] = $row;
+                }
+
+                for($i = $offset; $i < $items_per_page + $offset; $i++){
+                    if(empty($rows[$i])) continue;
+
+                    echo '<div class="post hvr-overline-from-left" onclick="location.href=\'post.php?id=' . $rows[$i]['id'] . '\';">
+                    <p class="post_title"><span class="glyphicon glyphicon-stop" style="margin-right:5px"></span>"' . $rows[$i]['article_title'] . '" <span style="font-weight: normal;">by ' . $rows[$i]['article_author'] . '</span></p>
+                    <p class="date">' . $rows[$i]['date'] . '</p>
+                    <p><span class="glyphicon glyphicon-th-list"></span> category: <a href="#">' . $rows[$i]['category'] . '</a><span class="glyphicon glyphicon-user"></span> author: <a href="#">' . $rows[$i]['username'] . '</a></p>
+                    <p><span class="glyphicon glyphicon-eye-open"></span> views: ' . $rows[$i]['views'] . '</p>
+                </div>';
+                }
+            }
+        }
+    ?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
