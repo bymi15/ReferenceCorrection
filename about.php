1965a0fae3776de08fa28250ad4f064333a871f5
Brian Min <bymi15@yahoo.com>
2016-04-10 04:26:08 +0100

    new description for about page

--- about.php
+++ about.php
@@ -44,13 +44,15 @@ my_session_start();
             <h2>References are important. Help us fix them.</h2>
             <p>How many times have you tried to chase up a reference only to find that the information is wrong? Or that the resulting paper does not justify the sentence? It is all too common and all too frustrating.</p><p>References are a fundamental part of a research paper but get very little attention. We all know they are important. They provide further information, justify comments and protect against plagiarism. But mistakes are frequent. A Cochrane review of referencing accuracy showed an average citation error rate of 38% (errors in the text details of the reference) and quotation error rate of 20% (errors of interpretation). (Wagner and Middleton, 2008)</p>
             <p style="font-weight: bold;">This website helps to fix those problems. Correct the mistakes and help others to find the information they need.</p>
+
+            <h2>How does this work?</h2>
+            <p>ReferenceChecker relies on the users to post references from medical research papers (uploaded in a .ris or .enw format). <p>Users can then view these posts and submit their suggestion on how to fix a certain incorrect reference</p><p>These suggestions can be viewed and rated (agree or disagree) by other users.</p><p>The suggestions with the highest ratings (agreements) is always displayed at the top, which means users can easily find the best correction of an erroneous reference.</p><p>Each reference will have a status bar (green, orange, red) which indicates the validity of the reference based on the number of suggestions it has and its net ratings. For instance, if a reference has no suggestions, it will be assumed correct and the status bar will be green.</p>
             <div class="text-center">
                 <a href="index.php" class="btn btn-primary">Back to Home <span class="glyphicon glyphicon-home" style="color:white"></a>
             </div>
         </div>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
